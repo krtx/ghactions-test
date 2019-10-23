bfb16354c89858661f40f31a3541b7d67029dc05
kinoshita-m <kinoshita-m@klab.com>
2019-10-23 14:25:21 +0900

    Initial Commit

---
+++ ghactions-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ghactions-test
+//
+//  Created by kinoshita-m on 2019/10/23.
+//  Copyright © 2019 krtx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
